c814b574a8503b88de6c31ef5f4b8fcdee259461
cfloresbr <cflores@biblioredes.gob.cl>
2024-05-29 16:26:51 -0400

    c

--- index.php
+++ index.php
@@ -18,6 +18,8 @@
 		<link href="inc/portfolio.css" rel="stylesheet" type="text/css" media="screen">
 		<link href="inc/contact.css" type="text/css" rel="stylesheet" media="screen">
 		<link rel="icon" type="image/png" href="img/shortcut.png"/>
+		<link href="inc/splide-4.1.3/dist/css/splide.min.css" rel="stylesheet">
+		<script src="inc/splide-4.1.3/dist/js/splide.min.js"></script>
 
 		<!-- Carrousel -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glider-js@1.7.8/glider.min.css">
@@ -108,6 +110,22 @@
 			<img width="100%" src="img/portada.png">
 		</div>
 		
+		<section class="splide" aria-label="Basic Structure Example">
+			<div class="splide__track">
+				<ul class="splide__list">
+				<li class="splide__slide"><img src="../img/slide1.png"></li>
+				<li class="splide__slide"><img src="../img/slide2.png"></li>
+				<li class="splide__slide"><img src="../img/slide3.png"></li>
+				</ul>
+			</div>
+		</section>
+		<script>
+			new Splide('.splide',{
+				type: 'loop',
+				autoplay: true,
+				interval: '4000'
+			}).mount();
+		</script>
 
 		<!-- Presentación y primera sección -->
 		<div id="showcase">
